4517f8a0df9b89ff7b38a396750f29c7d60107b6
taylordinkins <dinkinst@oregonstate.edu>
2022-05-02 19:25:42 -0700

    Adding normalization

--- src/xia_cpd.py
+++ src/xia_cpd.py
@@ -73,14 +73,67 @@ def perform_regression(data, kd=2):
         beta_hats_x.append(beta_hat_x_i)
         beta_hats_y.append(beta_hat_y_i)
     
+    # return should be (dim, dim-1) shape
+    # since we need one regression for each dim, and is vector size of dim-1
     return np.array(beta_hats_x), np.array(beta_hats_y)
 
-def calculate_residuals(beta_hat, data):
+def calculate_residuals(beta_hats, data):
+    # this is for one half of the data
+    N = data.shape[0]
+    dim = data.shape[1]
+    residuals = np.zeros((N, dim))
+    for k in range(N):
+        for i in range(dim):
+            X_m_i = np.delete(data, i, axis=1)
+            X_m_i_k = X_m_i[k, :]
+            X_i_k = data[k, i]
+            X_m_i_k_min_mu = X_m_i_k - np.mean(X_m_i.T, 1)
+            X_i_k_min_mu = X_i_k - np.mean(data[:, i])
+            pred = X_m_i_k_min_mu.dot(beta_hats[i])
+            epsilon = X_i_k_min_mu - pred
+            residuals[k, i] = epsilon
+    
+    return residuals
+
+def bias_corrected_residual_covariance(residuals, beta_hats):
+    residuals_cov = np.cov(residuals.T, bias=True)
+    dim = residuals_cov.shape[0]
+    residuals_cov_corrected = np.zeros(residuals_cov.shape)
+    for i in range(dim-1):
+        for j in range(i+1, dim):
+            first = residuals_cov[i, j]
+            second = residuals_cov[i, i]*beta_hats.T[i, j]
+            third = residuals_cov[j, j]*beta_hats.T[j-1, i]
+            residuals_cov_corrected[i, j] = -(first+second+third)
+            residuals_cov_corrected[j, i] = residuals_cov_corrected[i, j]
+    
+    return residuals_cov_corrected + np.diag(np.diag(residuals_cov))
+
+def calculate_T(residuals_cov_corrected):
+    dim = residuals_cov_corrected.shape[0]
+    T = np.zeros((dim, dim))
+    for i in range(dim):
+        for j in range(dim):
+            top = residuals_cov_corrected[i, j]
+            bottom = residuals_cov_corrected[i,i]*residuals_cov_corrected[j,j]
+            T[i, j] = top/bottom
+    
+    return T
+
+def calculate_theta(residuals_cov_corrected, beta_hats, N):
     """
     TODO
     """
+    dim = residuals_cov_corrected.shape[0]
+    theta = np.zeros((dim, dim))
 
-    pass
+    for i in range(dim):
+        for j in range(dim):
+            #top = (1 + beta_hats[])
+
+            pass
+
+
 
 
 def main():
@@ -88,9 +141,10 @@ def main():
     args = get_args()
     data = resolve_data(args)
     beta_hats_x, beta_hats_y = perform_regression(data)
-    print(beta_hats_x)
-    print(beta_hats_y)
-    
+    residuals_x = calculate_residuals(beta_hats_x, data[:1000, :])
+    residuals_x_cov_corrected = bias_corrected_residual_covariance(residuals_x, beta_hats_x)
+    T_x = calculate_T(residuals_x_cov_corrected)
+    print(T_x)
 
 
 
