428cfc64b632d84e3c24c926acae330bc86b3d8d
taylordinkins <dinkinst@oregonstate.edu>
2023-12-19 11:48:20 -0800

    Init different windows

--- sim_36.sh
+++ sim_36.sh
@@ -86,26 +86,35 @@
 
 ############################################################################
 
-#python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 6 --resid_type unstructured --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400
-#python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 10 --resid_type unstructured --step_size 1 --window_size 100 --M 5 --dim 40 --lam 5e-2 --N 400
-#python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 14 --resid_type unstructured --step_size 1 --window_size 100 --M 6 --dim 60 --lam 5e-2 --N 400
-#python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 18 --resid_type unstructured --step_size 1 --window_size 100 --M 8 --dim 80 --lam 5e-2 --N 400
+# python src/main.py --sim 1 --sim_type anderson_residual --num_indices 6 --resid_type unstructured --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 4 --dim 20 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2
+# python src/main.py --sim 1 --sim_type anderson_residual --num_indices 10 --resid_type unstructured --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 5 --dim 40 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2
+# python src/main.py --sim 1 --sim_type anderson_residual --num_indices 14 --resid_type unstructured --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 6 --dim 60 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2
+# python src/main.py --sim 1 --sim_type anderson_residual --num_indices 18 --resid_type unstructured --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 8 --dim 80 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2
+
+# python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 6 --resid_type unstructured --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400
+# python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 10 --resid_type unstructured --step_size 1 --window_size 100 --M 5 --dim 40 --lam 5e-2 --N 400
+# python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 14 --resid_type unstructured --step_size 1 --window_size 100 --M 6 --dim 60 --lam 5e-2 --N 400
+# python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 18 --resid_type unstructured --step_size 1 --window_size 100 --M 8 --dim 80 --lam 5e-2 --N 400
 
 # python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 6 --resid_type unstructured --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400 --burn_in 100
 # python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 10 --resid_type unstructured --step_size 1 --window_size 100 --M 5 --dim 40 --lam 5e-2 --N 400 --burn_in 100
 # python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 14 --resid_type unstructured --step_size 1 --window_size 100 --M 6 --dim 60 --lam 5e-2 --N 400 --burn_in 100
 # python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 18 --resid_type unstructured --step_size 1 --window_size 100 --M 8 --dim 80 --lam 5e-2 --N 400 --burn_in 100
 
+# python src/main.py --sim 1 --sim_type anderson_residual --num_indices 6 --resid_type block --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 4 --dim 20 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2
+# python src/main.py --sim 1 --sim_type anderson_residual --num_indices 10 --resid_type block --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 5 --dim 40 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2
+# python src/main.py --sim 1 --sim_type anderson_residual --num_indices 14 --resid_type block --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 6 --dim 60 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2
+# python src/main.py --sim 1 --sim_type anderson_residual --num_indices 18 --resid_type block --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 8 --dim 80 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2
 
-#python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 6 --resid_type block --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400
-#python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 10 --resid_type block --step_size 1 --window_size 100 --M 5 --dim 40 --lam 5e-2 --N 400
-#python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 14 --resid_type block --step_size 1 --window_size 100 --M 6 --dim 60 --lam 5e-2 --N 400
-#python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 18 --resid_type block --step_size 1 --window_size 100 --M 8 --dim 80 --lam 5e-2 --N 400
+# python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 6 --resid_type block --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400
+# python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 10 --resid_type block --step_size 1 --window_size 100 --M 5 --dim 40 --lam 5e-2 --N 400
+# python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 14 --resid_type block --step_size 1 --window_size 100 --M 6 --dim 60 --lam 5e-2 --N 400
+# python src/xia_cpd.py --sim 1 --sim_type anderson_residual --num_indices 18 --resid_type block --step_size 1 --window_size 100 --M 8 --dim 80 --lam 5e-2 --N 400
 
-#python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 6 --resid_type block --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400 --burn_in 100
-#python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 10 --resid_type block --step_size 1 --window_size 100 --M 5 --dim 40 --lam 5e-2 --N 400 --burn_in 100
-#python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 14 --resid_type block --step_size 1 --window_size 100 --M 6 --dim 60 --lam 5e-2 --N 400 --burn_in 100
-#python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 18 --resid_type block --step_size 1 --window_size 100 --M 8 --dim 80 --lam 5e-2 --N 400 --burn_in 100
+# python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 6 --resid_type block --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400 --burn_in 100
+# python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 10 --resid_type block --step_size 1 --window_size 100 --M 5 --dim 40 --lam 5e-2 --N 400 --burn_in 100
+# python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 14 --resid_type block --step_size 1 --window_size 100 --M 6 --dim 60 --lam 5e-2 --N 400 --burn_in 100
+# python src/kesh_cpd.py --sim 1 --sim_type anderson_residual --num_indices 18 --resid_type block --step_size 1 --window_size 100 --M 8 --dim 80 --lam 5e-2 --N 400 --burn_in 100
 
 ##############################################################################
 # python src/xia_cpd.py --sim 1 --sim_type cai_model_one --num_indices 6 --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400
@@ -136,20 +145,36 @@
 
 
 # UNSTRUCTURED RESIDS
-python src/main.py --sim 1 --sim_type orthogonal_mult_coeff --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 4 --dim 20 --optim_type Boyd --lam 3e-1 --N 400 --recursion 0 --linkage average --candidate_recursion 0 --base_M 2 --num_coeffs_change 2
-python src/main.py --sim 1 --sim_type orthogonal_mult_coeff --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 5 --dim 40 --optim_type Boyd --lam 3e-1 --N 400 --recursion 0 --linkage average --candidate_recursion 0 --base_M 4 --num_coeffs_change 2
-python src/main.py --sim 1 --sim_type orthogonal_mult_coeff --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 6 --dim 60 --optim_type Boyd --lam 3e-1 --N 400 --recursion 0 --linkage average --candidate_recursion 0 --base_M 6 --num_coeffs_change 2
-python src/main.py --sim 1 --sim_type orthogonal_mult_coeff --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 8 --dim 80 --optim_type Boyd --lam 3e-1 --N 400 --recursion 0 --linkage average --candidate_recursion 0 --base_M 8 --num_coeffs_change 2
+# python src/main.py --sim 1 --sim_type orthogonal_mult_coeff --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 4 --dim 20 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2 --num_coeffs_change 1 --log_pvals 0
+# python src/main.py --sim 1 --sim_type orthogonal_mult_coeff --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 5 --dim 40 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2 --num_coeffs_change 1 --log_pvals 0
+# python src/main.py --sim 1 --sim_type orthogonal_mult_coeff --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 6 --dim 60 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2 --num_coeffs_change 1 --log_pvals 0
+# python src/main.py --sim 1 --sim_type orthogonal_mult_coeff --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 8 --dim 80 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2 --num_coeffs_change 1 --log_pvals 0
 
-python src/kesh_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 4 --dim 20 --lam 3e-1 --N 400 --burn_in 100 --num_coeffs_change 2
-python src/kesh_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 5 --dim 40 --lam 3e-1 --N 400 --burn_in 100 --num_coeffs_change 2
-python src/kesh_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 6 --dim 60 --lam 3e-1 --N 400 --burn_in 100 --num_coeffs_change 2
-python src/kesh_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 8 --dim 80 --lam 3e-1 --N 400 --burn_in 100 --num_coeffs_change 2
+# python src/kesh_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400 --burn_in 100 --num_coeffs_change 1
+# python src/kesh_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 5 --dim 40 --lam 5e-2 --N 400 --burn_in 100 --num_coeffs_change 1
+# python src/kesh_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 6 --dim 60 --lam 5e-2 --N 400 --burn_in 100 --num_coeffs_change 1
+# python src/kesh_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 8 --dim 80 --lam 5e-2 --N 400 --burn_in 100 --num_coeffs_change 1
 
-python src/xia_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 4 --dim 20 --lam 3e-1 --N 400 --num_coeffs_change 2
-python src/xia_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 5 --dim 40 --lam 3e-1 --N 400 --num_coeffs_change 2
-python src/xia_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 6 --dim 60 --lam 3e-1 --N 400 --num_coeffs_change 2
-python src/xia_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 8 --dim 80 --lam 3e-1 --N 400 --num_coeffs_change 2
+# python src/xia_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400 --num_coeffs_change 1
+# python src/xia_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 5 --dim 40 --lam 5e-2 --N 400 --num_coeffs_change 1
+# python src/xia_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 6 --dim 60 --lam 5e-2 --N 400 --num_coeffs_change 1
+# python src/xia_cpd.py --sim 1 --sim_type orthogonal_mult_coeff --step_size 1 --window_size 100 --M 8 --dim 80 --lam 5e-2 --N 400 --num_coeffs_change 1
+
+
+#python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 4 --dim 20 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2 --sim_scale 0.2 --log_pvals 0
+#python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 5 --dim 40 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2 --sim_scale 0.2 --log_pvals 0
+#python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 6 --dim 60 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2 --sim_scale 0.2 --log_pvals 0
+#python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 100 --split_variance 0 --M 8 --dim 80 --optim_type Boyd --lam 5e-2 --N 400 --recursion 1 --linkage complete --candidate_recursion 1 --base_M 2 --sim_scale 0.2 --log_pvals 0
+
+#python src/kesh_cpd.py --sim 1 --sim_type orthogonal_small --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400 --burn_in 100 --sim_scale 0.2
+#python src/kesh_cpd.py --sim 1 --sim_type orthogonal_small --step_size 1 --window_size 100 --M 5 --dim 40 --lam 5e-2 --N 400 --burn_in 100 --sim_scale 0.2
+#python src/kesh_cpd.py --sim 1 --sim_type orthogonal_small --step_size 1 --window_size 100 --M 6 --dim 60 --lam 5e-2 --N 400 --burn_in 100 --sim_scale 0.2
+#python src/kesh_cpd.py --sim 1 --sim_type orthogonal_small --step_size 1 --window_size 100 --M 8 --dim 80 --lam 5e-2 --N 400 --burn_in 100 --sim_scale 0.2
+
+python src/xia_cpd.py --sim 1 --sim_type orthogonal_small --step_size 1 --window_size 100 --M 4 --dim 20 --lam 5e-2 --N 400 --sim_scale 0.2
+python src/xia_cpd.py --sim 1 --sim_type orthogonal_small --step_size 1 --window_size 100 --M 5 --dim 40 --lam 5e-2 --N 400 --sim_scale 0.2
+python src/xia_cpd.py --sim 1 --sim_type orthogonal_small --step_size 1 --window_size 100 --M 6 --dim 60 --lam 5e-2 --N 400 --sim_scale 0.2
+python src/xia_cpd.py --sim 1 --sim_type orthogonal_small --step_size 1 --window_size 100 --M 8 --dim 80 --lam 5e-2 --N 400 --sim_scale 0.2
 
 
 
