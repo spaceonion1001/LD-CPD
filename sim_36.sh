4590d702b02459916dfc1ab73c432c0ac59852d2
taylordinkins <dinkinst@oregonstate.edu>
2025-05-04 10:38:09 -0700

    Backups

--- sim_36.sh
+++ sim_36.sh
@@ -314,13 +314,13 @@
 
 
 ### TIMING RESULTS DOWN HERE
-python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 50 --split_variance 0 --M 4 --dim 20 --optim_type Boyd --lam 5e-2 --N 250 --recursion 1 --linkage single \
+python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 50 --split_variance 0 --M 4 --dim 20 --optim_type Boyd --lam 5e-2 --N 20 --recursion 1 --linkage single \
     --candidate_recursion 0 --base_M 2 --sim_scale 0.8 --log_pvals 1 --post_window_size 20 --train_percent 0.1 --thav 0 --thresh_const 0.8
-python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 50 --split_variance 0 --M 4 --dim 40 --optim_type Boyd --lam 5e-2 --N 250 --recursion 1 --linkage single \
+python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 50 --split_variance 0 --M 4 --dim 40 --optim_type Boyd --lam 5e-2 --N 20 --recursion 1 --linkage single \
     --candidate_recursion 0 --base_M 2 --sim_scale 0.8 --log_pvals 1 --post_window_size 20 --train_percent 0.1 --thav 0 --thresh_const 0.8
-python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 50 --split_variance 0 --M 4 --dim 60 --optim_type Boyd --lam 5e-2 --N 250 --recursion 1 --linkage single \
+python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 50 --split_variance 0 --M 4 --dim 60 --optim_type Boyd --lam 5e-2 --N 20 --recursion 1 --linkage single \
     --candidate_recursion 0 --base_M 2 --sim_scale 0.8 --log_pvals 1 --post_window_size 20 --train_percent 0.1 --thav 0 --thresh_const 0.8
-python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 50 --split_variance 0 --M 4 --dim 80 --optim_type Boyd --lam 5e-2 --N 250 --recursion 1 --linkage single \
+python src/main.py --sim 1 --sim_type orthogonal_small --single_test 0 --step_size 1 --window_size 50 --split_variance 0 --M 4 --dim 80 --optim_type Boyd --lam 5e-2 --N 20 --recursion 1 --linkage single \
     --candidate_recursion 0 --base_M 2 --sim_scale 0.8 --log_pvals 1 --post_window_size 20 --train_percent 0.1 --thav 0 --thresh_const 0.8
 
 
